434b0d32d7bb8c888f2c87de11effe74d9d04094
SergioGalindoCruz <100978144+SergioGalindoCruz@users.noreply.github.com>
2023-09-11 20:28:18 -0500

    Actualización Credenciales

--- conexion.php
+++ conexion.php
@@ -11,4 +11,18 @@
     }else{
         //echo "La conexión a la base de datos ha sido exitosa.";
     }
+/*
+    define('Servidor', 'localhost:3306');
+    define('Usuario', 'root');
+    define('Password', '');
+    define('Database', 'softwareinventory');
+
+    $conectar = mysqli_connect(Servidor, Usuario, Password, Database);
+
+    if($conectar===FALSE){
+        die("Revise los datos de conexión.<br> Ha habido un error: ".mysqli_connect_error());
+    }else{
+        //echo "La conexión a la base de datos ha sido exitosa.";
+    }
+*/
 ?>
